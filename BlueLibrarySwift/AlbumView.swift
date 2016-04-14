032e96702bcd5c115dd00db6aa580ca5a211e486
Seto Elkahfi <setoelkahfi@gmail.com>
2016-04-14 20:07:51 +0700

    Add Album model and AlbumView

---
+++ BlueLibrarySwift/AlbumView.swift
@@ -0,0 +1,53 @@
+//
+//  AlbumView.swift
+//  BlueLibrarySwift
+//
+//  Created by SDMobile on 4/14/16.
+//  Copyright © 2016 Raywenderlich. All rights reserved.
+//
+
+import UIKit
+
+class AlbumView: UIView {
+    
+    private var coverImage: UIImageView!
+    private var indicator: UIActivityIndicatorView!
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        commonInit()
+    }
+    
+    init(frame: CGRect, albumCover: String) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    func commonInit() {
+        backgroundColor = UIColor.blackColor()
+        coverImage = UIImageView(frame: CGRect(x: 5, y: 5, width: frame.size.width - 10, height: frame.size.height - 10))
+        addSubview(coverImage)
+        indicator = UIActivityIndicatorView()
+        indicator.center = center
+        indicator.activityIndicatorViewStyle = .WhiteLarge
+        indicator.startAnimating()
+        addSubview(indicator)
+    }
+    
+    func highlightAlbum(didHighlightView didHighlightView: Bool) {
+        if didHighlightView == true {
+            backgroundColor = UIColor.whiteColor()
+        } else {
+            backgroundColor = UIColor.blackColor()
+        }
+    }
+
+}
